c148f84c4c54a01283106a10898dad44b3ffded6
amoksh9191 <73216205+amoksh9191@users.noreply.github.com>
2021-05-16 21:22:20 +0530

    Update __init__.py

--- Basic-Py-Maths-Library-master/__init__.py
+++ Basic-Py-Maths-Library-master/__init__.py
@@ -39,6 +39,22 @@ def difference(a,b):
 
 ###############################
 
+def fact_loop(num):
+    if num < 0:
+        return 0
+    if num == 0:
+        return 1
+
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial = factorial * i
+    print(factorial)
+    
+################################
+
+
+
+
 
 
 
