3c2034d44c8d49c9247dfde12ef53c8a4d9f4820
utkarsh222004 <113412904+utkarsh222004@users.noreply.github.com>
2023-10-13 14:07:29 +0530

    I have added age calculation formulae

--- Basic-Py-Maths-Library-master/__init__.py
+++ Basic-Py-Maths-Library-master/__init__.py
@@ -201,6 +201,14 @@ def log(a):
     return(log(a))
 
 ################################
+from datetime import date
+
+def calculate_age(born):
+    today = date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+################################
+
 
 
 
